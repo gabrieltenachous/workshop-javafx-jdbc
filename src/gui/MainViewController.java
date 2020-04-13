8c6f4c16b3b3b440f487eb6769b985e130c6b108
Gabriel Oliveira <gabriel_oliveira_f_sousa@outlook.com>
2020-04-13 18:18:47 -0300

    Passing a Department object to DepartmentForm view

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -31,12 +31,12 @@ public class MainViewController implements Initializable {
 
 	@FXML
 	public void onMenuItemSellerAction() {
-		System.out.println("OnMenuAction");
+		System.out.println("onMenuItemSellerAction");
 	}
 
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		loadView("/gui/DepartmentList.fxml", (DepartmentListController controller)->{
+		loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) -> {
 			controller.setDepartmentService(new DepartmentService());
 			controller.updateTableView();
 		});
@@ -44,18 +44,18 @@ public class MainViewController implements Initializable {
 
 	@FXML
 	public void onMenuItemAboutAction() {
-		loadView("/gui/About.fxml",x ->{});
+		loadView("/gui/About.fxml", x -> {
+		});
 	}
 
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
-
 	}
 
-	private synchronized <T> void loadView(String absoluteName,Consumer<T> initializingAction) {
+	private synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-			VBox newVbox = loader.load();
+			VBox newVBox = loader.load();
 
 			Scene mainScene = Main.getMainScene();
 			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
@@ -63,15 +63,12 @@ public class MainViewController implements Initializable {
 			Node mainMenu = mainVBox.getChildren().get(0);
 			mainVBox.getChildren().clear();
 			mainVBox.getChildren().add(mainMenu);
-			mainVBox.getChildren().addAll(newVbox.getChildren());
-			
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+
 			T controller = loader.getController();
 			initializingAction.accept(controller);
-			
 		} catch (IOException e) {
 			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
 		}
-
 	}
-
 }
